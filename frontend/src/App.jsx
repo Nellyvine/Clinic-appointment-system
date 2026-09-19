4929d83b489a370cbc2af42c3f7bc5f7c6545f42
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-09-19 05:05:18 +0400

    Implement routing and add Navbar component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+
 function App() { 
-    return <h1>It is working</h1>; 
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<h1>Dashboard</h1>} />
+                <Route path="/patients" element={<h1>Patients</h1>} />
+                <Route path="/doctors" element={<h1>Doctors</h1>} />
+                <Route path="/appointments" element={<h1>Appointments</h1>} />
+            </Routes>
+        </div>
+    ); 
 } 
 
 export default App;
